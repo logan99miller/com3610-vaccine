56e42ec8406316a341159f3e197ad3b8a2efb5b6
Logan Miller <lmiller6@sheffield.ac.uk>
2022-02-21 10:56:20 +0000

    Added methods to calculate distance between 2 locations

--- src/StockLevel.java
+++ src/StockLevel.java
@@ -4,6 +4,41 @@ import java.util.HashMap;
 
 public class StockLevel {
 
+    // Applies the Haversine formula to calculate the shortest distance over the earth's surface
+    // Avoid use of cosine formula as it is unreliable for small distances (https://www.themathdoctors.org/distances-on-earth-2-the-haversine-formula/
+    public static double getDistance(double longitudeA, double latitudeA, double longitudeB, double latitudeB) {
+        final double EARTH_RADIUS = 6371; // in km
+        double longitudeDifferenceRad = degreesToRadians(longitudeB - longitudeA);
+        double latitudeDifferenceRad = degreesToRadians(latitudeB - latitudeA);
+        double latitudeARad = degreesToRadians(latitudeA);
+        double latitudeBRad = degreesToRadians(latitudeB);
+
+        // a represents the square of half the chord length between the points
+        double a =
+            0.5 - Math.cos(latitudeDifferenceRad) / 2 +
+            Math.cos(latitudeARad) * Math.cos(latitudeBRad) *
+            (1 - Math.cos(longitudeDifferenceRad)) / 2;
+
+        double angularDistance = 2 * Math.asin(Math.sqrt(a));
+
+        return EARTH_RADIUS * angularDistance;
+    }
+
+    // Applies the Haversine formula to calculate the shortest distance over the earth's surface
+    // Avoid use of cosine formula as it is unreliable for small distances (https://www.themathdoctors.org/distances-on-earth-2-the-haversine-formula/)
+    public static double getDistance(HashMap<String, Object> locationA, HashMap<String, Object> locationB) {
+//    public static double getDistance() {
+        double longitudeA = Double.parseDouble((String) locationA.get("Location.longitude"));
+        double latitudeA = Double.parseDouble((String) locationA.get("Location.latitude"));
+        double longitudeB = Double.parseDouble((String) locationB.get("Location.longitude"));
+        double latitudeB = Double.parseDouble((String) locationB.get("Location.latitude"));
+        return getDistance(longitudeA, latitudeA, longitudeB, latitudeB);
+    }
+
+    private static double degreesToRadians(double degrees) {
+        return degrees * (Math.PI / 180);
+    }
+
     // Should be modified to store vaccine in most suitable fridge by sorting lifespans and picking fridge with longest lifespan
     // Remember to keep original algorithm for comparison
     public static void updateFactoryStockLevels(Data data, int updateRate, int simulationSpeed) {
